4fd4da8f2d72a8536dba98dc5f2c429ee8b0b5e4
Ross Golder <ross@golder.org>
2025-05-26 22:47:20 +0700

    Fixes for 16.0.

--- l10n_th_account_statement_import_kbiz/__manifest__.py
+++ l10n_th_account_statement_import_kbiz/__manifest__.py
@@ -1,12 +1,12 @@
 {
     "name": "Import Kasikorn KBiz Bank Statement",
     "category": "Banking addons",
-    "version": "16.0.1.0.1",
+    "version": "16.0.1.0.2",
     "license": "AGPL-3",
     "author": "Ross Golder, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-thailand",
     "depends": [
-        "account_statement_import",
+        "account_statement_import_base",
     ],
     "data": [
         "views/view_account_statement_import.xml",
